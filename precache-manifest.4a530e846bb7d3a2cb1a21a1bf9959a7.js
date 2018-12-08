9e028721a0dee7c86745bc0f0ab88add8f3c17dd
Zacarias Torrezão <z.torrezao@gmail.com>
2018-12-08 13:24:08 +0200

    Updates

---
+++ precache-manifest.4a530e846bb7d3a2cb1a21a1bf9959a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ae787cae9a757d651f4",
+    "url": "/facedetect-ui/static/css/main.3e342a11.chunk.css"
+  },
+  {
+    "revision": "1ae787cae9a757d651f4",
+    "url": "/facedetect-ui/static/js/main.1ae787ca.chunk.js"
+  },
+  {
+    "revision": "a7f6aee236eab859b545",
+    "url": "/facedetect-ui/static/css/1.ca97e298.chunk.css"
+  },
+  {
+    "revision": "a7f6aee236eab859b545",
+    "url": "/facedetect-ui/static/js/1.a7f6aee2.chunk.js"
+  },
+  {
+    "revision": "dd9dd8781b26201b5816",
+    "url": "/facedetect-ui/static/js/runtime~main.dd9dd878.js"
+  },
+  {
+    "revision": "73da97b6bb4932dd5344dedc53336313",
+    "url": "/facedetect-ui/index.html"
+  }
+];
